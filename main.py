08483c675cfaebf737e7578e6c03a4ebf83f5616
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-03-10 15:15:17 +0000

    done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from rembg import remove
 from PIL import Image
 
-inputImg = "man.jpg"
-outputImage = "urIMGwithotBg.png"
+inputImg = "man.jpg"   # your image name goes here
+outputImage = "urIMGwithotBg.png"    # what do you wanna call it 
 
 imgOpener = Image.open(inputImg)
 output_image = remove(imgOpener)
